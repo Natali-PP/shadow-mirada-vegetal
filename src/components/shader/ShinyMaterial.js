d56e88ff40319d2668276dd55779c30438f4f7de
Karen Palacio <karen.palacio.1994@gmail.com>
2021-09-04 18:13:28 -0300

    change shader, leave old one commented out

--- src/components/shader/ShinyMaterial.js
+++ src/components/shader/ShinyMaterial.js
@@ -1,9 +1,9 @@
 import * as THREE from "three"
 import { shaderMaterial } from "@react-three/drei"
 import { extend } from "@react-three/fiber"
-
+//0.5337
 const ShinyMaterial = shaderMaterial(
-  { baseSpeed: 0.05, noiseTexture: null, noiseScale: 0.5337, alpha: 1.0, time: 10.0 },
+  { baseSpeed: 0.05, noiseTexture: null, noiseScale: 0.1, alpha: 1.0, time: 10.0 },
   `
   varying vec2 vUv;
   void main() {
@@ -23,7 +23,9 @@ const ShinyMaterial = shaderMaterial(
   varying vec2 vUv;
   void main()
   {
-    vec2 uvTimeShift = vUv + vec2( -0.3, 1.5 ) * time * baseSpeed;
+    vec2 uvTimeShift = mix(vUv, vUv*vec2( -0.1, 0.9 ) * time * baseSpeed,time*.5); 
+    //viejo:
+    //vec2 uvTimeShift = vUv + vec2( -0.3, 1.5 ) * time * baseSpeed;
     vec4 noiseGeneratorTimeShift = texture2D( noiseTexture, uvTimeShift );
     vec2 uvNoiseTimeShift = vUv + noiseScale * vec2( noiseGeneratorTimeShift.r, noiseGeneratorTimeShift.b );
     vec4 baseColor = texture2D( baseTexture, uvNoiseTimeShift );
